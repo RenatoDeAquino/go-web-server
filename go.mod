a6a7afd2d1edc21eb15fee7e6eefa38bc5db6085
Renato Aquino <repitriano12@gmail.com>
2021-08-29 12:44:08 -0300

    finishing some web-server on go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module RenatoDeAquino/go-web-serverhello-example
+
+go 1.13
+
+require github.com/gorilla/mux v1.8.0
---
+++ hello-web.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello World! \n")
+}
+
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	return r
+}
+
+func main() {
+	router := router()
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:9100",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
